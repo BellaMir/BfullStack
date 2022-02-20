d60d0ebdfd56d847c4f947c8c797fda962ce714b
BellaMir <bellacontrerascamperos@yahoo.es>
2022-02-20 12:18:55 +0100

    programa pirámide en JS

---
+++ main.js
@@ -0,0 +1,22 @@
+/*const todo = ['cantar', 'bailar','cocinar','estudiar','ver tv'];
+
+for (let i = 0; i < todo.length; i++){
+    const element = todo[i];
+    console.log(i);
+    console.log(element);
+
+}*/
+
+
+  //console.log(parking);
+ 
+  // Recorremos el primer array utilizando i como posición
+  for (var i = 1; i <= 9; i++) {
+  
+      
+    for (var j = 0; j < i; j++) {
+      document.write(i);
+    }
+    document.write("</br>");   
+  }
+ 
